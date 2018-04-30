8839f9797a32b06a577b3f6eaaf06697f0bbec89
Paskal <djpascimix@gmail.com>
2018-05-01 01:17:11 +0800

    cleanup

---
+++ src/js/main.js
@@ -0,0 +1,86 @@
+"use strict";
+
+
+jQuery(document).ready(function ($) {
+
+
+    /*---------------------------------------------*
+     * Mobile menu
+     ---------------------------------------------*/
+    $(document).on('click', '.navbar-collapse.in', function (e) {
+        if ($(e.target).is('a')) {
+            $(this).collapse('hide');
+        }
+    });
+
+    /*---------------------------------------------*
+     * STICKY scroll
+     ---------------------------------------------*/
+
+    $.localScroll();
+
+
+
+    /*---------------------------------------------*
+     * WOW
+     ---------------------------------------------*/
+
+    var wow = new WOW({
+        mobile: false // trigger animations on mobile devices (default is true)
+    });
+    wow.init();
+
+
+    /*---------------------------------------------*
+     * Counter 
+     ---------------------------------------------*/
+
+    $('.statistic-counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+
+
+
+
+
+
+
+
+// main-menu-scroll
+
+    jQuery(window).scroll(function () {
+        var top = jQuery(document).scrollTop();
+        var height = 300;
+        //alert(batas);
+
+        if (top > height) {
+            jQuery('.navbar-fixed-top').addClass('menu-scroll');
+        } else {
+            jQuery('.navbar-fixed-top').removeClass('menu-scroll');
+        }
+    });
+
+//    $('#menu').slicknav();
+
+    $('#mixcontent').mixItUp({
+        animation: {
+            animateResizeContainer: false,
+            effects: 'fade rotateX(-45deg) translateY(-10%)'
+        }
+    });
+
+
+
+
+    //End
+});
+
+
+
+
+
+
+
