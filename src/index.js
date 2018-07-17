51d3e7c42c7905c7245c0c13b3ba700320910aee
Paskal <djpascimix@gmail.com>
2018-07-17 16:38:35 +0800

    Remove HTTPS from app server (use reverse proxy)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 var fs = require('fs');
 var http = require('http');
-var https = require('https');
-var privateKey  = fs.readFileSync('../../private.key', 'utf8');
-var certificate = fs.readFileSync('../../thebutton_co.crt', 'utf8');
-
-var credentials = {key: privateKey, cert: certificate};
 
 var express = require('express');
 var app = express();
@@ -16,7 +11,5 @@ app.get('/', function (req, res) {
 });
 
 var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(8080);
-httpsServer.listen(8443);
