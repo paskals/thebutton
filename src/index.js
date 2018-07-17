fcc21671b78c6c4820a89e6309345b10077d580a
Paskal <djpascimix@gmail.com>
2018-07-17 13:58:36 +0800

    Add SSL and small fixes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var privateKey  = fs.readFileSync('./private.key', 'utf8');
-var certificate = fs.readFileSync('./thebutton_co.crt', 'utf8');
-
-var credentials = {key: privateKey, cert: certificate};
-
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -15,8 +7,4 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './index.html'));
 });
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(8080);
-httpsServer.listen(8443);
+app.listen(8080, function (){ console.log('Example app listening on port 3000!'); })
